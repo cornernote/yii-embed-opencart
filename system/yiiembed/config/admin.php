32ae1e6036d4f71e250fe56734830262de4d3839
Brett O'Donnell <cornernote@gmail.com>
2013-12-14 16:00:37 +1030

    Create admin.php

---
+++ system/yiiembed/config/admin.php
@@ -0,0 +1,10 @@
+<?php
+return array(
+    'modules' => array(
+        'gii' => array(
+            'class' => 'yiiembed.modules.gii.OcGiiModule',
+            'ipFilters' => array('127.0.0.1'),
+            'password' => false,
+        ),
+    ),
+);
